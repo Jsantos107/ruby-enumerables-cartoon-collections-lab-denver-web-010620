8f2d08d320856f46b69a352e5b06a390e84103a4
Jorge Santos <neneluis55@gmail.com>
2019-12-29 08:21:00 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,4 +1,4 @@
-def roll_call_dwarves(array)# code an argument here
+def roll_call_dwarves(array)
     i = 0 
   while i < array.length 
     puts "#{i + 1}. #{array[i]}"
@@ -6,8 +6,8 @@ def roll_call_dwarves(array)# code an argument here
   end 
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  
 end
 
 def long_planeteer_calls# code an argument here
